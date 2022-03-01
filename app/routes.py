41f3b1af5701a6bf0f9c7fcd9f67f9200c3f0944
Kiptoo Dennis <denysmutay@gmail.com>
2022-03-01 15:15:08 +0600

    the route for my about page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from app import app
-from flask import render_template, url_for
+from flask import render_template, url_for, request, redirect
+
 
 @app.route('/contact')
 def contact():
-    return render_template(url_for('contact'))
+    return render_template('contact.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
